b3b727471c1e585c9f1fe3f6df5530144f4eff0e
brent-edwards-online <brent@brentedwardsonline.com>
2017-05-11 08:52:02 +1000

    Initial Commit

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new Movie();
+            movie.Id = 0;
+            movie.Name = "Bob the Builder";
+            return View(movie);
+        }
+    }
+}
